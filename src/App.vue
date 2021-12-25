8baa50ee504568159a66b84c1076301c897c87e6
richex-cn <hzxtiger@vip.qq.com>
2021-12-25 23:45:57 +0800

    add pug dependency

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import       HelloWorld       from './components/HelloWorld.vue'
 </script>
 
 <!-- <template> -->
@@ -16,7 +16,7 @@ hello-world(         msg="hello"   )/
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir,         Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
